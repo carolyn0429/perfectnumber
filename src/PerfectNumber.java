129afd74142f46e4377cdd34fa1536ea862cd006
Carolyn Hung <carolynhung@Carolyns-MacBook-Pro-2.local>
2018-03-22 21:06:44 -0700

    add iterative solution of finding perfect number in range

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -5,24 +5,36 @@ import java.util.Scanner;
  */
 public class PerfectNumber {
 
-    public static void isAPerfectNumber(int num) {
-        int sum = 0;
-        for (int i=1; i<num; i++) {
-            if(num % i == 0) {
-                sum += i;
+    private static void perfectNumberInRange(int lower, int upper) {
+        int count = 0;
+        int sum, j;
+        for (int i = lower; i<=upper; i++) {
+            sum = 0;
+            for (j = 1 ; j < i; j++) {
+                if (i % j == 0) {
+                    sum += j;
+                }
+            }
+            if (sum == i) {
+                count ++;
+                System.out.println(i + " is a perfect number.");
             }
         }
-        if (sum == num) {
-            System.out.println(num + " is a perfect number.");
+        if(count == 0){
+            System.out.println("There is no perfect numbers in range " + lower + " and " + upper);
+        } else if(count == 1) {
+            System.out.println("There is " + count + " perfect number in range " + lower + " and " + upper);
         } else {
-            System.out.println(num + " is not a perfect number");
+            System.out.println("There are " + count + "perfect numbers in range " + lower + " and " + upper);
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a number: \n");
-        int num = scanner.nextInt();
-        isAPerfectNumber(num);
+        System.out.println("Please enter a lower bound number: \n");
+        int lowerBound = scanner.nextInt();
+        System.out.println("Please enter a upper bound number: \n");
+        int upperBound = scanner.nextInt();
+        perfectNumberInRange(lowerBound, upperBound);
     }
 }
